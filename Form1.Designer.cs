051d088f69da07b39c447bad5e84a218f2b916c7
PchelinAndrej <andrej.pchelin@gmail.com>
2023-03-10 23:32:52 +0500

    Перенос из соседнего репозитория

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(547, 325);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolStripComboBox1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(570, 32);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.deleteAllToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 28);
+            this.fileToolStripMenuItem.Text = "file";
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.createToolStripMenuItem.Text = "create";
+            this.createToolStripMenuItem.Click += new System.EventHandler(this.createToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Text = "save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.deleteToolStripMenuItem.Text = "delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // deleteAllToolStripMenuItem
+            // 
+            this.deleteAllToolStripMenuItem.Name = "deleteAllToolStripMenuItem";
+            this.deleteAllToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.deleteAllToolStripMenuItem.Text = "delete all";
+            this.deleteAllToolStripMenuItem.Click += new System.EventHandler(this.deleteAllToolStripMenuItem_Click);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 28);
+            this.toolStripComboBox1.TextChanged += new System.EventHandler(this.toolStripComboBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 372);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Notes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        public System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteAllToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string path;
+
+        async private void VisualizingNote(string path_note)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(path_note))
+                {
+                    string text = await reader.ReadToEndAsync();
+                    richTextBox1.Text = text;
+                }
+            }
+            catch
+            {
+                // При попытке редактирования пути к файлу
+                toolStripComboBox1.Text = "";
+            }
+        }
+
+        async private void SavingNote(string pathNote, string text)
+        {
+            // Сохранение заметки
+            using (StreamWriter writer = new StreamWriter(pathNote, false))
+            {
+                await writer.WriteLineAsync(text);
+            }
+        }
+
+        private void FirstNote(string pathNote)
+        {
+            string text = "Привет!\nЭто твоя первая заметка!\nМожешь удалить её и создать свою.\nУдачи!";
+            string path_note = path + "1.txt";
+
+            richTextBox1.Text = text;
+            
+            SavingNote(path_note, text);
+
+            toolStripComboBox1.Items.Add(path_note);
+            toolStripComboBox1.Text = path_note;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.path = AppDomain.CurrentDomain.BaseDirectory.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolStripComboBox1.Items.AddRange(Directory.GetFiles(this.path, "*.txt"));
+            if (toolStripComboBox1.Items.Count == 0)
+            {
+                FirstNote(this.path);
+            }
+        }
+
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripComboBox1.Text = "";
+            this.richTextBox1.Text = "";
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Сохранение файла
+
+            string path_note = toolStripComboBox1.Text;
+            string text = richTextBox1.Text;
+            
+            if (string.IsNullOrEmpty(path_note))
+            {
+                // Создание нового
+                if (toolStripComboBox1.Items.Count == 0)
+                {
+                    // Если заметок ещё нет
+                    path_note = path + "1.txt";
+                }
+                else
+                {
+                    // Если заметки уже были
+                    int note_name = toolStripComboBox1.Items.Count + 1; 
+                    path_note = path + note_name.ToString() + ".txt";                    
+                }
+                
+                SavingNote(path_note, text);
+
+                toolStripComboBox1.Items.Add(path_note);
+                toolStripComboBox1.Text = path_note;
+            }
+            else
+            {
+                // Редактирование существующего файла
+                SavingNote(path_note, text);
+            }            
+        }
+
+        private void toolStripComboBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Вывод файла на экран
+            string path_note = toolStripComboBox1.Text;
+            if (!string.IsNullOrEmpty(path_note))
+            {           
+                VisualizingNote(path_note);
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Удаление заметки
+            string path_note = toolStripComboBox1.Text;
+
+            richTextBox1.Text = "";
+            toolStripComboBox1.Items.Remove(path_note);
+            toolStripComboBox1.Text = "";
+
+            if (!string.IsNullOrEmpty(path_note))
+            {
+                FileInfo fileInf = new FileInfo(path_note);
+                if (fileInf.Exists)
+                {
+                    fileInf.Delete();
+                }
+            }
+        }
+
+        private void deleteAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            toolStripComboBox1.Items.Clear();
+            toolStripComboBox1.Text = "";
+            System.IO.DirectoryInfo di = new DirectoryInfo(this.path);
+            foreach (FileInfo note in di.EnumerateFiles("*.txt"))
+            {
+                note.Delete();
+            }
+        }
+    }
+}
